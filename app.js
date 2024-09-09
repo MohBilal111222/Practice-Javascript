4fddffcb3b27f60b26780999545d9931e61e0fde
Bilal <mohammadbilal91112@gmail.com>
2024-09-10 04:46:04 +0500

    learn js
    learn js

---
+++ app.js
@@ -0,0 +1,63 @@
+
+alert("hello");
+
+// function sayMyName(){
+//     console.log("bilal");
+
+// }
+
+
+
+// sayMyName ()
+
+
+// function addTwoNumbers(number1 , number2){
+
+// let result = number1 + number2 
+// return result  
+
+// return number1 + number2
+// }
+
+// const result = addTwoNumbers(55 , 78)
+// console.log(" Result");
+
+
+
+// function loginUserMessege(username){
+//      if(username === ("bilal")){
+//         console.log("please enter a username");
+//      }
+//     return `${username} just logged in`
+// }
+
+
+// console.log(loginUserMessege("bilal"));
+
+
+// console.log(calcculateCarPrice(200 , 400 , 500));
+
+
+
+// const user = {
+//     username : "bilal",
+//     age : 19
+// }
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and age is ${anyobject.age}`);
+
+// }
+
+// handleObject(user)
+
+
+
+
+
+// const myNewArray = {200 , 300 , 400 , 500}
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
+
+// console.log(returnSecondValue{[200 , 300 ,400 , 500]});
