03ba5339036c46dbdaf953980ed64b6d5eb6c2aa
Bilal <mohammadbilal91112@gmail.com>
2024-09-12 05:03:47 +0500

    Conversion of data types

--- 5-6 (Datatype)/app.js
+++ 5-6 (Datatype)/app.js
@@ -1,1 +1,17 @@
-alert('3+3')
+// alert('3+3')
+
+
+ // i a using browser not nodejs
+
+
+
+ // console.log("bilal");
+
+
+
+ let someNumber = 44
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber);
+ 
