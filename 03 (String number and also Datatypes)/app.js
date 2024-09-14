38661ed5d6ee6aa5e37e9cba5a95d615153afaa7
Bilal <mohammadbilal91112@gmail.com>
2024-09-15 04:34:45 +0500

    changes

--- 03 (String number and also Datatypes)/app.js
+++ 03 (String number and also Datatypes)/app.js
@@ -1,4 +1,26 @@
-console.log("1" + 2 + 2);
+// console.log("1" + 2 + 2);
 
 
-console.log(1 + 2 + 2 + "3");
+// console.log(1 + 2 + 2 + "3");
+
+
+
+
+
+
+
+
+
+
+const user = {
+    username : "bilal",
+    age : 19
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and age is ${anyobject.age}`);
+
+}
+
+handleObject(user)
+
